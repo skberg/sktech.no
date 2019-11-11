a185e10f94e8b797208901e3cb11afd4e0323856
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2019-11-11 12:17:56 +0100

    Sktech.no

---
+++ hoevdside.js
@@ -0,0 +1,31 @@
+
+// sidebar 
+
+function Tvist(x) {
+    x.classList.toggle("change");
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-109444342-1');
+
+
+
+
+
+
+
+// to the top butten 
+
+
